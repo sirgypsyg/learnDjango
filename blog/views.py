44a47c289e7ce2ebe02ea0c1ea6f47e07c433874
gypsyg <siemagosciu32@gmail.com>
2024-08-17 15:40:34 +0200

    added Database

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
+
 
 posts = [ 
     {
@@ -18,7 +20,7 @@ posts = [
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
