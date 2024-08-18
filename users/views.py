85a2633603ab1f58f61ad1ee3a2d6eb00fe97d58
gypsyg <siemagosciu32@gmail.com>
2024-08-18 22:30:03 +0200

    logout working for now...

--- users/views.py
+++ users/views.py
@@ -7,9 +7,8 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Your account has been created, you can now log in !')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
