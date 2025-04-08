6493798e3267080308d8aa9a261668878966bc49
gypsyg <siemagosciu32@gmail.com>
2025-04-08 21:18:34 +0200

    Moved templates to same place and fixed "currently" word in profile update

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
+from django.forms.widgets import ClearableFileInput
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -17,7 +18,14 @@ class UserUpdateForm(forms.ModelForm):
         model = User
         fields = ['username', 'email']
 
+class NoClearableFileInput(ClearableFileInput):
+    template_name = 'widgets/no_clearable_file_input.html'
+
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['image']
+        widgets = { #its all to delete "currently" word in progile
+            'image': NoClearableFileInput(attrs={'class': 'form-control-file'})
+        }
+
